18f83b75dd82559f237d7c4e34b887fb520c277b
vilicwh <vilicwh@outlook.com>
2019-10-31 13:59:57 +0800

    Site updated: 2019-10-31 13:59:53

--- install/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -x 
-dir=`dirname $0`
-
-[[ -d $dir/../../public/images/ ]] && cp $dir/my_avatar.jpg $dir/../../public/images/ && :
-[[ -d $dir/../../public/js/src/ ]] && cp $dir/love.js $dir/../../public/js/src/ && :
-
